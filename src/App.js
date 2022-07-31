6a96906b2e38e375056f32147f021fff91d03eb6
Jesús Álvarez <khisus19@gmail.com>
2022-07-30 21:12:34 -0500

    Reamining numbers and operators buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Btn from './components/Button';
-import freeCodeCampLogo from './img/fcc_primary_large.svg'
+import Screen from './components/Screen';
+import freeCodeCampLogo from './img/fcc_primary_large.svg';
 
 function App() {
   return (
@@ -12,14 +13,31 @@ function App() {
           alt='Freecodecamp logo' />
       </div>
       <div className='calculator-container'>
+        <Screen />
         <div className='row'>
           <Btn>1</Btn>
+          <Btn>2</Btn>
+          <Btn>3</Btn>
           <Btn>+</Btn>
         </div>
         <div className='row'>
+          <Btn>4</Btn>
+          <Btn>5</Btn>
+          <Btn>6</Btn>
+          <Btn>-</Btn>
         </div>
-        <div className='row'></div>
-        <div className='row'></div>
+        <div className='row'>
+          <Btn>7</Btn>
+          <Btn>8</Btn>
+          <Btn>9</Btn>
+          <Btn>*</Btn>
+        </div>
+        <div className='row'>
+          <Btn>=</Btn>
+          <Btn>0</Btn>
+          <Btn>.</Btn>
+          <Btn>/</Btn>
+        </div>
         <div className='row'></div>
       </div>
     </div>
